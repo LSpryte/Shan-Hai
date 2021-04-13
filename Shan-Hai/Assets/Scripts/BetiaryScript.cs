5dcc09fe021059f26c1431c9c727627bd2fbaf77
Bowen <bhe13vince@gmail.com>
2021-04-12 17:51:01 -0700

    Bowen add GetKeyDown code

---
+++ Shan-Hai/Assets/Scripts/BetiaryScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetiaryScript : MonoBehaviour
+{
+    public GameObject Bestiary;
+    public bool isBestiaryOpen = false;
+    public GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.B))
+        { 
+            Bestiary.gameObject.SetActive(!Bestiary.gameObject.activeSelf); 
+        }
+    }
+    public void OpenCloseBestiary()
+    {
+        //if the bestiary isn't open (in normal gameplay)
+        if (isBestiaryOpen == false)
+        {
+            Bestiary.SetActive(true); //open the bestiary
+            gameManager.PauseGame(); //pause the game so player can not continue to move
+            isBestiaryOpen = true;
+        }
+        else //if the bestiary is already open
+        {
+            Bestiary.SetActive(false); //close the bestiary
+            gameManager.UnpauseGame(); //unpause the game so the player may move
+            isBestiaryOpen = false;
+        }
+    }
+}
---
+++ Shan-Hai/Assets/Scripts/InteractionScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionScript : MonoBehaviour
+{
+    public bool CanInteract = false;
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "NPC")
+        {
+            CanInteract = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        CanInteract = false;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && CanInteract) 
+        {
+            Debug.Log("Talking to ; ");
+        }
+    }
+}
